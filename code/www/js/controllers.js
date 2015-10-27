21eb330a11586cb6c618e2eefa055da2beca91ef
Sheryl Boughton <sherpbo@gmail.com>
2015-10-27 11:34:03 -0700

    play song samples, include play, pause functionality, deletions from favorites

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -2,46 +2,52 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, User) {
-  $scope.songs = [
-      {
-         "title":"Stealing Cinderella",
-         "artist":"Chuck Wicks",
-         "image_small":"https://i.scdn.co/image/d1f58701179fe768cff26a77a46c56f291343d68",
-         "image_large":"https://i.scdn.co/image/9ce5ea93acd3048312978d1eb5f6d297ff93375d"
-      },
-      {
-         "title":"Venom - Original Mix",
-         "artist":"Ziggy",
-         "image_small":"https://i.scdn.co/image/1a4ba26961c4606c316e10d5d3d20b736e3e7d27",
-         "image_large":"https://i.scdn.co/image/91a396948e8fc2cf170c781c93dd08b866812f3a"
-      },
-      {
-         "title":"Do It",
-         "artist":"Rootkit",
-         "image_small":"https://i.scdn.co/image/398df9a33a6019c0e95e3be05fbaf19be0e91138",
-         "image_large":"https://i.scdn.co/image/4e47ee3f6214fabbbed2092a21e62ee2a830058a"
-      }
-   ];
+.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations, $ionicLoading) {
 
-   $scope.currentSong = angular.copy($scope.songs[0]);
+   Recommendations.init()
+    .then(function(){
+      $scope.currentSong = Recommendations.queue[0];
+      hideLoading();
+      Recommendations.playCurrentSong();
+    });
 
-   $scope.sendFeedback = function (bool) {
-     if (bool) User.addSongToFavorites($scope.currentSong);
+  $scope.sendFeedback = function (bool) {
+    if (bool) {
+      User.addSongToFavorites($scope.currentSong);
+    }
 
-     $scope.currentSong.favorited = bool;
-     $scope.currentSong.hide = true;
+    $scope.currentSong.favorited = bool;
+    $scope.currentSong.hide = true;
+    Recommendations.nextSong();
 
     $timeout(function(){
+      $scope.currentSong = Recommendations.queue[0];
+    }, 250);
 
-     // set the current song to one of our three songs
-     var randomSong = Math.round(Math.random() * ($scope.songs.length - 1));
+    Recommendations.playCurrentSong();
 
-     // update current song in scope
-     $scope.currentSong = angular.copy($scope.songs[randomSong]);
-   }, 250);
+  };
 
- };
+  $scope.nextAlbumImg = function() {//a little hack for caching the next image in advance for faster image loading
+    if (Recommendations.queue.length > 1) {//called in "discover" view; note the css for img-lookahead
+      return Recommendations.queue[1].image_large;
+    }
+
+    return '';
+  };
+
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  };
+
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  };
+
+  showLoading();
 
 })
 
@@ -60,6 +66,15 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, Recommendations) {
+  $scope.enteringFavorites = function() {
+    Recommendations.haltAudio();
+  };
+
+
+  $scope.leavingFavorites = function() {
+    console.log('deselectedd');
+    Recommendations.init();
+  };
 
 });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -6,20 +6,72 @@ angular.module('songhop.services', [])
   };
 
   o.addSongToFavorites = function(song) {
-    // make sure there's a song to add
     if (!song) return false;
 
-    // add to favorites array
     o.favorites.unshift(song);
   };
 
   o.removeSongFromFavorites = function(song, index) {
-    // make sure there's a song to add
     if (!song) return false;
 
-    // add to favorites array
     o.favorites.splice(index, 1);
   };
 
   return o;
+})
+
+.factory('Recommendations', function($http, SERVER, $q) {
+  var media;
+
+  var o = {
+    queue: []
+  };
+
+  o.init = function() {
+    if (o.queue.length === 0) {
+      return o.getNextSongs();
+
+    } else {
+      return o.playCurrentSong();
+    }
+  };
+
+  o.getNextSongs = function() {
+    return $http({
+      method: 'GET',
+      url: SERVER.url + '/recommendations'
+    }).success(function(data){
+      o.queue = o.queue.concat(data);
+    });
+  };
+
+  o.nextSong = function() {
+    o.haltAudio();
+
+    o.queue.shift();
+
+    if (o.queue.length <= 3) {
+      o.getNextSongs();
+    }
+  };
+
+  o.playCurrentSong = function() {
+    var defer = $q.defer();
+
+    media = new Audio(o.queue[0].preview_url);
+
+    media.addEventListener("loadeddata", function() {
+      defer.resolve();
+    });
+
+    media.play();
+
+    return defer.promise;
+  };
+
+  o.haltAudio = function() {
+    if (media) media.pause();
+  };
+  return o;
+
 });
